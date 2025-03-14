adbbff591afa9442b0799687bd6a7c117a1de973
Tiago Azevedo <tiagoadesouza21@gmail.com>
2025-03-14 00:04:07 -0300

    MudnanÃças iniciais

--- src/publicabiblioteca/sistema/dominio/Livro.java
+++ src/publicabiblioteca/sistema/dominio/Livro.java
@@ -1,4 +1,60 @@
 package publicabiblioteca.sistema.dominio;
 
 public class Livro {
+    private String nomeLivro;
+    private int anoLancamentoLivro;
+    private String sinopseLivro;
+    private String nomeAutor;
+    private String sobreAutor;
+
+    //construtor
+    public Livro(String criarNovoLivro) {
+        this.setNomeLivro(criarNovoLivro);
+    }
+
+    public String getNomeLivro(){
+        return nomeLivro;
+    }
+
+    public String getSinopseLivro(){
+        return sinopseLivro;
+    }
+
+    public int getAnoLancamentoLivro(){
+        return anoLancamentoLivro;
+    }
+
+    public String getNomeAutor() {
+        if (nomeAutor == null || nomeAutor.isEmpty()) {
+            return "Desconhecido";
+        } else {
+            return nomeAutor;
+        }
+    }
+
+    public String getSobreAutor() {
+        return sobreAutor;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public void setAnoLancamentoLivro(int anoLancamentoLivro) {
+        this.anoLancamentoLivro = anoLancamentoLivro;
+    }
+
+    public void setSinopseLivro(String sinopseLivro) {
+        this.sinopseLivro = sinopseLivro;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public void setSobreAutor(String sobreAutor) {
+        this.sobreAutor = sobreAutor;
+    }
 }
+
+
--- src/publicabiblioteca/sistema/padrao/Usuario.java
+++ src/publicabiblioteca/sistema/padrao/Usuario.java
@@ -2,8 +2,69 @@ package publicabiblioteca.sistema.padrao;
 
 import publicabiblioteca.sistema.dominio.Livro;
 
-public class ContemLivros {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Usuario {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Livro livroDracula = new Livro("Dracula");
+        livroDracula.setAnoLancamentoLivro(1998);
+        livroDracula.setSinopseLivro("Vampiro pohaaa");
+        livroDracula.setNomeAutor("Claudio");
+
+        Livro livroVerity = new Livro("Verity");
+        livroVerity.setAnoLancamentoLivro(2019);
+        livroVerity.setSinopseLivro("Sinistro pra poha a mulher la fingindo que ta paralisada");
+
+        ArrayList<Livro> listaDeLivros = new ArrayList<Livro>();
+        listaDeLivros.add(livroDracula);
+        listaDeLivros.add(livroVerity);
+
+        for (Livro livro : listaDeLivros) {
+            System.out.println("Titulo: "+livro.getNomeLivro());
+            System.out.println("Autor: "+livro.getNomeAutor());
+            System.out.println("Sinopse: "+livro.getSinopseLivro());
+            System.out.println("Ano de Lançamento: "+livro.getAnoLancamentoLivro()+"\n");
+        }
+
+        while (true) {
+            System.out.println("1. Adicionar Livro");
+            System.out.println("2. Lista de livros");
+            System.out.println("3. Sair");
+            System.out.print("\nDigite a opção: ");
+            if (scan.hasNextInt()){
+                int escolha = scan.nextInt();
+                scan.nextLine();
+                if (escolha == 1){
+                    System.out.print("Digite o nome do livro: ");
+                    String criarNovoLivro = scan.nextLine();
+                    Livro novoLivro = new Livro(criarNovoLivro); //construtor
+                    System.out.print("Digite o nome do Autor: ");
+                    novoLivro.setNomeAutor(scan.nextLine());
+                    System.out.print("Digite o ano de lançamento do livro: ");
+                    novoLivro.setAnoLancamentoLivro(scan.nextInt());
+                    scan.nextLine();
+                    System.out.print("Escreva a sinopse do livro: ");
+                    novoLivro.setSinopseLivro(scan.nextLine());
+                    System.out.println("\n"+novoLivro.getNomeLivro()+"foi adicionado a lista!");
+
+                    listaDeLivros.add(novoLivro);
+                } else if (escolha == 2) {
+                    for (Livro livro : listaDeLivros) {
+                        System.out.println("Titulo: "+livro.getNomeLivro());
+                        System.out.println("Autor: "+livro.getNomeAutor());
+                        System.out.println("Sinopse: "+livro.getSinopseLivro());
+                        System.out.println("Ano de Lançamento: "+livro.getAnoLancamentoLivro()+"\n");
+                    }
+                } else if (escolha == 3) {
+                    break;
+                }
+            } else {
+                System.out.println("Digite a opção certa!");
+            }
+        }
 
     }
 
